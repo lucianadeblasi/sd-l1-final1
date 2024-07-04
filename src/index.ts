df966f9bb563ae7109ffece62c0dee0317960ce9
Luciana Deblasi <lu.deblasi@hotmail.com>
2024-07-04 13:02:16 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
-import * as minimist from "minimist";
+import * as minimist from 'minimist';
+import { PelisController } from './controller';
 
-function parseaParams(argv) {
-  const resultado = minimist(argv);
+async function main() {
+  const args = minimist(process.argv.slice(2));
+  const controller = new PelisController();
 
-  return resultado;
-}
-
-function main() {
-  const params = parseaParams(process.argv.slice(2));
-
-  console.log(params);
+  if (args._[0] === 'add') {
+    const peli = {
+      id: args.id,
+      title: args.title,
+      tags: args.tags
+    };
+    const result = await controller.add(peli);
+    console.log(result ? 'Peli agregada' : 'Error al agregar la peli');
+  } else if (args._[0] === 'get') {
+    const peli = await controller.get({ id: parseInt(args._[1]) });
+    console.log(peli);
+  } else if (args._[0] === 'search') {
+    const searchOptions = { title: args.title, tag: args.tag };
+    const pelis = await controller.get({ search: searchOptions });
+    console.log(pelis);
+  } else {
+    const pelis = await controller.get();
+    console.log(pelis);
+  }
 }
 
 main();
