18e1c8018751bdcf0c608d47f48103abac4fbed9
Luciana Deblasi <lu.deblasi@hotmail.com>
2024-07-04 13:01:33 -0300

    Update controllers.ts

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,25 @@
-import { PelisCollection, Peli } from "./models";
+import { PelisCollection, Peli, SearchOptions } from './models';
 
 class PelisController {
-  constructor() {}
+  private collection: PelisCollection;
+
+  constructor() {
+    this.collection = new PelisCollection();
+  }
+
+  async get(options?: { id?: number; search?: SearchOptions }): Promise<Peli | Peli[]> {
+    if (options?.id) {
+      return this.collection.getById(options.id);
+    }
+    if (options?.search) {
+      return this.collection.search(options.search);
+    }
+    return this.collection.getAll();
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    return this.collection.add(peli);
+  }
 }
+
 export { PelisController };
